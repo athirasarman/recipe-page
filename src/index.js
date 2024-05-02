57a5d3aa543c7249d675a5291f311d57ee3484d5
Athira K S <athiraks@Athiras-MacBook-Air.local>
2024-05-02 11:47:00 +0530

    Bug Fixes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import { Route, Link } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
   <script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
     <App />
-    </Router>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
