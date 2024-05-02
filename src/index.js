6095e51e8766fbe856c6d5cb6f2ef15c961275e8
Athira K S <athiraks@Athiras-MacBook-Air.local>
2024-05-02 11:43:36 +0530

    Bug FIxes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { Route, Link } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
+  <script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
     <App />
-  </React.StrictMode>
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
